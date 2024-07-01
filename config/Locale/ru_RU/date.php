259165fec25b2f96509e376c81aeacfc4b2dc55e
Andrei Guliaikin <guland2000@users.noreply.github.com>
2024-07-01 13:04:58 +0200

    Create date.php

---
+++ config/Locale/ru_RU/date.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * FoodCoopShop - The open source software for your foodcoop
+ *
+ * Licensed under the GNU Affero General Public License version 3
+ * For full copyright and license information, please see LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @since         FoodCoopShop 2.1.0
+ * @license       https://opensource.org/licenses/AGPL-3.0
+ * @author        Mario Rothauer <office@foodcoopshop.com>
+ * @copyright     Copyright (c) Mario Rothauer, https://www.rothauer-it.com
+ * @link          https://www.foodcoopshop.com
+ */
+return [
+    'DateFormat' => [
+        'DateShort' => 'ММ/дд/гг',
+        'DateLong2' => 'ММ/дд/гггг',
+        'DateNTimeShort' => 'ММ/дд/г ЧЧ:мм',
+        'DateNTimeShort2' => 'ММ/дд/гг ЧЧ:мм',
+        'DateNTimeLongWithSecs' => 'ММ/дд/г ЧЧ:мм:сс',
+        'TimeShort' => 'ЧЧ:мм',
+        'DateForDatepicker' => 'мм/дд/гг',
+        'Year' => 'Г',
+        'DateShortAlt' => 'м/д/Г'
+    ]
+];
+?>
